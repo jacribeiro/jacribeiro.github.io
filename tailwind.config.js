d7836d058e6e854d13ccc961100407e91397c74a
jacribeiro <j.alberto.cribeiro@gmail.com>
2024-12-18 12:26:43 +0000

    Performed last changes on the body.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         'light': '#F4F7F5', 
         'highlight': '#F3B61F' //azul - 80A4ED amarelo - f3b61f
       },
+      boxShadow: {
+        'yellowGlow': '0 0 5px 5px rgba(243, 182, 31, 0.4)',
+        'experienceBox': '0 -8px 6px -3px rgba(0, 0, 0, 0.1)',
+      }
     },
   },
   plugins: [],
