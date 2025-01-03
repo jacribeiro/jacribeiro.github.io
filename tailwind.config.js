abeb982f7499960dba2bf927c6ee6601786171a1
jacribeiro <j.alberto.cribeiro@gmail.com>
2025-01-03 17:59:40 +0000

    Completed website design. Installed Prettier on the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html"],
   theme: {
     extend: {
       fontFamily: {
-        header: 'Merriweather, serif',
-        main: 'Roboto Condensed, sans'
+        header: "Merriweather, serif",
+        main: "Roboto Condensed, sans",
       },
       colors: {
-        'dark': '#504746',
-        'medium': '#6D615F',
-        'light': '#F4F7F5', 
-        'highlight': '#F3B61F' //azul - 80A4ED amarelo - f3b61f
+        dark: "#504746",
+        medium: "#6D615F",
+        light: "#F4F7F5",
+        highlight: "#F3B61F",
       },
       boxShadow: {
-        'yellowGlow': '0 0 5px 5px rgba(243, 182, 31, 0.4)',
-        'experienceBox': '0 -8px 6px -3px rgba(0, 0, 0, 0.1)',
-      }
+        yellowGlow: "0 0 5px 5px rgba(243, 182, 31, 0.4)",
+        experienceBox: "0 -8px 6px -3px rgba(0, 0, 0, 0.1)",
+      },
     },
   },
   plugins: [],
-}
-
+};
